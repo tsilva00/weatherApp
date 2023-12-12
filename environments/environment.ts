2618e9c83b926e25306dd11d783eebd3c000918d
Thais Helena <73835477+tsilva00@users.noreply.github.com>
2023-12-12 13:39:38 -0300

    Update environment.ts

--- environments/environment.ts
+++ environments/environment.ts
@@ -7,9 +7,7 @@ export const environment = {
   weatherApiBaseUrl : 'https://openweather43.p.rapidapi.com/weather',
   XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
   XRapidAPIHostHeaderValue: 'openweather43.p.rapidapi.com',
-	XRapidAPIKeyHeatherName: 'X-RapidAPI-Key',
-  ,
-
+  XRapidAPIKeyHeatherName: 'X-RapidAPI-Key'
 };
 
 /*
